a1509a60ff8580f652d70ca1ee20c5e7601214cf
jhsware <sebastian@urbantalk.se>
2016-10-27 18:21:30 +0200

    Added BoolField (WIP) and updated tests

--- test/test-formGenerator.js
+++ test/test-formGenerator.js
@@ -4,6 +4,7 @@
 
 // Testing libraries
 const expect = require('chai').expect
+const $ = require('cheerio')
 
 
 // Our code...
@@ -14,7 +15,8 @@ const { IDisplayFieldWidget } = require('../lib').interfaces
 
 const simpleSchema = new Schema('Simple Schema', {
     title: validators.textField({}),
-    age: validators.integerField({})
+    age: validators.integerField({}),
+    happy: validators.boolField({})
 })
 
 const nestedSchema = new Schema('Nested Schema', {
@@ -25,18 +27,32 @@ const nestedSchema = new Schema('Nested Schema', {
 describe('renderForm', function () {
   it('can render a simple schema', function () {
     var outp = renderFormFields({
-      data: {},
+      data: {
+          title: 'A great one',
+          age: 42,
+          happy: true
+      },
       formSchema: simpleSchema
     })
     expect(outp).not.to.equal(undefined)
+    const formData = $('<form>' + outp + '</form>').serializeArray()
+    expect(formData[0].name).to.equal('title')
+    expect(formData[0].value).to.equal('A great one')
+    expect(formData[3].name).to.equal('happy')
+    expect(formData[3].value).to.equal('true')
   })
   
   it('can render a nested schema', function () {
     var outp = renderFormFields({
-      data: {simple: {}},
+      data: {simple: {
+          happy: false
+      }},
       formSchema: nestedSchema
     })
     expect(outp).not.to.equal(undefined)
+    const formData = $('<form>' + outp + '</form>').serializeArray()
+    expect(formData[3].name).to.equal('simple[happy]__exists__')
+    expect(formData[3].value).to.equal('marker')
   })
 
   it('can render a nested schema with IDisplayFieldWidget', function () {
